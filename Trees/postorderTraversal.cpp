bc42b4989beef6a449aa7bcda89933c3c658d06f
manshisharma1372 <manshisharma1372@gmail.com>
2023-08-18 19:51:56 +0530

    tree day 2

---
+++ Trees/postorderTraversal.cpp
@@ -0,0 +1,20 @@
+
+
+void postorder(TreeNode* root, vector<int> &ans){
+    if(root==NULL){
+        return;
+    }
+
+    postorder(root->left,ans);
+    postorder(root->right,ans);
+    ans.push_back(root->data);
+    
+}
+vector<int> postorderTraversal(TreeNode *root)
+{
+    // Write your code here.
+
+    vector<int> ans;
+    postorder(root,ans);
+    return ans;
+}
